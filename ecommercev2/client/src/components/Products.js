394cc69b14cfff9dff194f9da670df72c256790e
ymercedr <yareline.mercedr@gmail.com>
2023-01-01 22:17:38 -0400

    price filter buttons added and work

--- ecommercev2/client/src/components/Products.js
+++ ecommercev2/client/src/components/Products.js
@@ -5,6 +5,7 @@ import "./Products.css";
 export default function Products() {
   const [books, setBooks] = useState([]);
 
+  //Default Use Effect
   useEffect(() => {
     const fetchAllBooks = async () => {
       try {
@@ -17,8 +18,76 @@ export default function Products() {
     };
     fetchAllBooks();
   }, []);
+
+  //Filter By Price
+  const filterResult = (price) => {
+    const fetchFilteredPrice = async () => {
+      try {
+        const res = await axios.get(`http://localhost:3001/filter/${price}`);
+        setBooks(res.data);
+        console.log(res);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchFilteredPrice();
+  };
+
+  // USEFUL AUTHOR
+  // const filterAuthor = (author) => {
+  //   const fetchFilteredAuthor = async () => {
+  //     try {
+  //       const res = await axios.get(`http://localhost:3001/filter/${author}`);
+  //       setBooks(res.data);
+  //       console.log(res);
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
+  //   fetchFilteredAuthor();
+  // };
+
+  //INITIAL IF ELSE FILTER
+  // if (filter === "lessThanTen") {
+  //   const fetchLessThanTen = async () => {
+  //     try {
+  //       const res = await axios.get(`http://localhost:3001/${filter}`);
+  //       setBooks(res.data);
+  //       console.log(res);
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
+  //   fetchLessThanTen();
+  // } else if (filter === "lessThanFifteen") {
+  //   const fetchLessThanFifteen = async () => {
+  //     try {
+  //       const res = await axios.get(`http://localhost:3001/${filter}`);
+  //       setBooks(res.data);
+  //       console.log(res);
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
+  //   fetchLessThanFifteen();
+
   return (
     <main>
+      <div>
+        <form>
+          {/* onSubmit={() => filterAuthor()} */}
+          <label for="author">Search Authors</label>
+          <br />
+          <input type="text" name="author" placeholder="Enter Name"></input>
+        </form>
+        {/* <label for="title">Search Titles</label>
+        <br />
+        <input type="text" name="title" placeholder="Enter Title"></input> */}
+      </div>
+      <div>
+        <button onClick={() => filterResult("10.00")}>Less Than $10!</button>
+        <button onClick={() => filterResult("15.00")}>Less Than $15!</button>
+      </div>
       <div className="book-container">
         {books.map((book) => (
           <div className="book" key={book.productId}>
--- ecommercev2/server/index.js
+++ ecommercev2/server/index.js
@@ -1,7 +1,6 @@
 //Creating the server
 const express = require("express");
 const mysql = require("mysql");
-// const db = require("./config/db");
 const cors = require("cors");
 
 const app = express();
@@ -22,12 +21,55 @@ app.get("/", (req, res) => res.json("Hello this is the backend."));
 
 //Route to get all products
 app.get("/books", (req, res) => {
-  const q = "SELECT * from products_list";
+  const q = "SELECT * FROM products_list";
   db.query(q, (err, data) => {
     if (err) return res.json(err);
     return res.json(data);
   });
 });
+
+//Route to get all products x amount or less
+app.get("/filter/:amount", (req, res) => {
+  const price = req.params.amount;
+  // console.log(req.params.amount);
+  // console.log(price);
+  const q = `SELECT * FROM products_list WHERE productPrice <= ${price}`;
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+//USEFULL AUTHOR
+//Route to get all books from a specific author
+// app.get("/filter/:author", (req, res) => {
+//   const aName = req.params.author;
+//   console.log(req.params.author);
+//   // console.log(price);
+//   const q = `SELECT * FROM products_list WHERE productAuthor = ${aName}`;
+//   db.query(q, (err, data) => {
+//     if (err) return res.json(err);
+//     return res.json(data);
+//   });
+// });
+
+//INITIAL ROUTES TO GET ALL PRODUCTS $10/$15 OR LESS
+// app.get("/lessThanTen", (req, res) => {
+//   const q = "SELECT * FROM products_list WHERE productPrice <= 10.00";
+//   db.query(q, (err, data) => {
+//     if (err) return res.json(err);
+//     return res.json(data);
+//   });
+// });
+
+// // Route to get all products $10 or less
+// app.get("/lessThanFifteen", (req, res) => {
+//   const q = "SELECT * FROM products_list WHERE productPrice <= 15.00";
+//   db.query(q, (err, data) => {
+//     if (err) return res.json(err);
+//     return res.json(data);
+//   });
+// });
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
